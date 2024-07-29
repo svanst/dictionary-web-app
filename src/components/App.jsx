d638b828ed18d3412976ed7b40aaa4458eba4e34
svanst <144538702+svanst@users.noreply.github.com>
2024-07-29 14:07:13 +0200

    add global styling and utility classes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+// import styles from "../global-styles/utilities.module.css";
+
 export default function App() {
   return <h1>React app</h1>;
 }
---
+++ src/script.jsx
@@ -0,0 +1,10 @@
+import "./global-styles/reset.module.css";
+import "./global-styles/root.module.css";
+import "./global-styles/utilities.module.css";
+
+import { createRoot } from "react-dom/client";
+import App from "./components/App.jsx";
+
+const root = createRoot(document.querySelector("#root"));
+
+root.render(<App />);
